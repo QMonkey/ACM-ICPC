edfd0e6187dde90cc2edf5c139ed6cd389fbffdb
QMKQMonkey <QMKQMonkey@gmail.com>
2013-11-09 14:31:03 +0800

    Add hanoi and integer_division

---
+++ hanoi/hanoi.h
@@ -0,0 +1,17 @@
+#ifndef HANOI_H
+#define HANOI_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+extern void hanoi(int32_t level,int src,int auxiliary,int dest);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
